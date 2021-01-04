3ce739384342aa149036ffdd8aee0ac86cd8abc2
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-04 17:44:42 +0100

    Adding eureka service

---
+++ EurekaService/src/test/java/rs/uns/acs/ftn/EurekaService/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.uns.acs.ftn.EurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
