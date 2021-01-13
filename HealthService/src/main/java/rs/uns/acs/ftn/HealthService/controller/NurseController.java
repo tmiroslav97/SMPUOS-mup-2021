eec5be52451ce1e693238471888773e689ecd3e9
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-13 19:44:51 +0100

    Adding initial nurse controller

---
+++ HealthService/src/main/java/rs/uns/acs/ftn/HealthService/controller/NurseController.java
@@ -0,0 +1,21 @@
+package rs.uns.acs.ftn.HealthService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.uns.acs.ftn.HealthService.model.Nurse;
+import rs.uns.acs.ftn.HealthService.service.NurseService;
+import rs.uns.acs.ftn.HealthService.service.impl.NurseServiceImpl;
+
+@RestController
+@RequestMapping(value = "/nurse")
+public class NurseController extends AbstractRESTController<Nurse, String> {
+
+    private NurseService nurseService;
+
+    @Autowired
+    public NurseController(NurseServiceImpl nurseServiceImpl) {
+        super(nurseServiceImpl);
+        this.nurseService = nurseServiceImpl;
+    }
+}
