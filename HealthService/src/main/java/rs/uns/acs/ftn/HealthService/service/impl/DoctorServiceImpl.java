1a9ae20d5ca86477b7849e3c32b77b09091c49cc
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-13 19:07:13 +0100

    Adding initial doctor service

---
+++ HealthService/src/main/java/rs/uns/acs/ftn/HealthService/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,20 @@
+package rs.uns.acs.ftn.HealthService.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.uns.acs.ftn.HealthService.model.Doctor;
+import rs.uns.acs.ftn.HealthService.repository.DoctorRepository;
+import rs.uns.acs.ftn.HealthService.service.AbstractCRUDService;
+import rs.uns.acs.ftn.HealthService.service.DoctorService;
+
+@Service
+public class DoctorServiceImpl extends AbstractCRUDService<Doctor, String> implements DoctorService {
+
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    public DoctorServiceImpl(DoctorRepository doctorRepository) {
+        super(doctorRepository);
+        this.doctorRepository = doctorRepository;
+    }
+}
