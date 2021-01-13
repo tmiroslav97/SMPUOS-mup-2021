16a8a0694c041432bcc271f642abe8cdaba9517b
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-13 19:47:54 +0100

    Adding initial appointment request controller

---
+++ HealthService/src/main/java/rs/uns/acs/ftn/HealthService/controller/AppointmentRequestController.java
@@ -0,0 +1,21 @@
+package rs.uns.acs.ftn.HealthService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rs.uns.acs.ftn.HealthService.model.AppointmentRequest;
+import rs.uns.acs.ftn.HealthService.service.AppointmentRequestService;
+import rs.uns.acs.ftn.HealthService.service.impl.AppointmentRequestServiceImpl;
+
+@RestController
+@RequestMapping(value = "/app-req")
+public class AppointmentRequestController extends AbstractRESTController<AppointmentRequest, String> {
+
+    private AppointmentRequestService appointmentRequestService;
+
+    @Autowired
+    public AppointmentRequestController(AppointmentRequestServiceImpl appointmentRequestServiceImpl) {
+        super(appointmentRequestServiceImpl);
+        this.appointmentRequestService = appointmentRequestServiceImpl;
+    }
+}
