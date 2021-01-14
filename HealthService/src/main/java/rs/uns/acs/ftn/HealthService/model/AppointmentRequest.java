0613fc81fceddb54e3f06dcf6a648ed42e73ed2a
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-14 13:12:49 +0100

    Adding missing getter and setter annotation and adding json getter and setter for date

--- HealthService/src/main/java/rs/uns/acs/ftn/HealthService/model/AppointmentRequest.java
+++ HealthService/src/main/java/rs/uns/acs/ftn/HealthService/model/AppointmentRequest.java
@@ -1,11 +1,22 @@
 package rs.uns.acs.ftn.HealthService.model;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Setter
+@Getter
 @Document
 public class AppointmentRequest {
 
@@ -33,5 +44,16 @@ public class AppointmentRequest {
         this.lastName = lastName;
         this.email = email;
         this.date = date;
+    }
+
+    @JsonGetter("date")
+    public String getTheDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(this.date);
+    }
+
+    @JsonSetter("date")
+    public void setTheDate(String date) throws ParseException {
+        this.date = new SimpleDateFormat("yyyy-MM-dd").parse(date);
     }
 }
