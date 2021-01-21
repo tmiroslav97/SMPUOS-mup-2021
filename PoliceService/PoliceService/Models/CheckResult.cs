49266a53a9ad2d3df1d4ba54ba40376520bfca8c
JovanJovkic <51271444+JovanJovkic@users.noreply.github.com>
2021-01-21 21:33:01 +0100

    backend funkcionalnosti

---
+++ PoliceService/PoliceService/Models/CheckResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoliceService.Models
+{
+    public class CheckResult
+    {
+        public bool res { get; set; }
+    }
+}
---
+++ PoliceService/PoliceService/Models/WeaponLicence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoliceService.Models
+{
+    public class WeaponLicence
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public DateTime ValidUntil { get; set; }
+    }
+}
