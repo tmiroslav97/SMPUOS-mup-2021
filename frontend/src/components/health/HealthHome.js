c2b35c00c2faa9819e0952aad8d3ab6b11dd72e4
Miroslav Tomic <tomic.miroslav97@gmail.com>
2021-01-22 15:14:12 +0100

    Refactoring frontend

--- frontend/src/components/health/HealthHome.js
+++ frontend/src/components/health/HealthHome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Container} from 'react-bootstrap';
+import { Row, Col, Container, Card, Button } from 'react-bootstrap';
 import { history } from '../../index';
 
 const HealthHome = () => {
@@ -10,7 +10,32 @@ const HealthHome = () => {
                     <center><h1 className="mb-5">Zdravstveni servisi početna</h1></center>
                 </Col>
             </Row>
-            
+            <Row className="mb-3">
+                <Col md={{ span: 4, offset: 2 }} xs={12} >
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="imgs/nurse.jpg" />
+                        <Card.Body>
+                            <Card.Title>Medicinsko osoblje</Card.Title>
+                            <Card.Text>
+                                Pristupite funkcionalnostima medicinskog osoblja.
+                            </Card.Text>
+                            <Button variant="link" onClick={() => { history.push('/health/nurse') }}>Otvori</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={{ span: 4, offset: 2 }} xs={12}>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="imgs/doctor.jpg" />
+                        <Card.Body>
+                            <Card.Title>Doktori</Card.Title>
+                            <Card.Text>
+                                Pristupite funcionalnostima doktora.
+                            </Card.Text>
+                            <Button variant="link" onClick={() => { history.push('/health/doctor') }}>Otvori</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </Container>
     );
 }
