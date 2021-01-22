1cf0ce9b035ad5a4ee0f7113f2259b4c94ed341c
JovanJovkic <51271444+JovanJovkic@users.noreply.github.com>
2021-01-22 13:52:39 +0100

    ostatak funkcionalnosti

---
+++ PoliceService/PoliceService/Models/PersonCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoliceService.Models
+{
+    public class PersonCheck
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string uid { get; set; }
+
+        public string fathersName { get; set; }
+    }
+}
